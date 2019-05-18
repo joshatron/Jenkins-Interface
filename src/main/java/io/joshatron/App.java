9358a84565eb02259f8b2d92e2dfdaf01e4c02a2
Joshua Leger <legermjoshua@gmail.com>
2019-05-17 21:00:25 -0700

    Initialized maven

---
+++ src/main/java/io/joshatron/App.java
@@ -0,0 +1,13 @@
+package io.joshatron;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
