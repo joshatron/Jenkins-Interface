7f599b89eeb0b50dc738be6897d30e19cb069c9e
Joshua Leger <legermjoshua@gmail.com>
2019-05-21 08:07:36 -0700

    Started filling out body of job building

--- src/main/java/io/joshatron/jenkins/operations/Builder.java
+++ src/main/java/io/joshatron/jenkins/operations/Builder.java
@@ -3,7 +3,13 @@ package io.joshatron.jenkins.operations;
 import io.joshatron.jenkins.config.JenkinsConfig;
 import io.joshatron.jenkins.config.JenkinsJob;
 import io.joshatron.jenkins.config.JenkinsServer;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
 
+import java.io.IOException;
 import java.util.List;
 
 public class Builder {
@@ -12,10 +18,26 @@ public class Builder {
         throw new IllegalStateException("This is a utility class");
     }
 
-    public static void buildJob(JenkinsServer server, JenkinsJob job, Arguments arguments) {
+    public static void buildJob(JenkinsServer server, JenkinsJob job, Arguments arguments) throws IOException {
+        HttpClient client = HttpClients.createDefault();
+
+        String url = server.getBaseUrl() + job.getUrl() + "/build";
+        String auth = server.getUsername() + ":" + server.getPassword();
+        String payload = createPayload(job, arguments);
+
+        HttpPost request = new HttpPost(url);
+        request.setHeader("Authorization", auth);
+        StringEntity entity = new StringEntity(payload, ContentType.APPLICATION_JSON);
+        request.setEntity(entity);
+
+        client.execute(request);
     }
 
-    public static void buildJobs(JenkinsConfig config, Arguments arguments) {
+    private static String createPayload(JenkinsJob job, Arguments arguments) {
+        return "";
+    }
+
+    public static void buildJobs(JenkinsConfig config, Arguments arguments) throws IOException {
         for(JenkinsServer server : config.getServers()) {
             for(JenkinsJob job : server.getJobs()) {
                 buildJob(server, job, arguments);
@@ -23,11 +45,11 @@ public class Builder {
         }
     }
 
-    public static void buildJobs(JenkinsConfig config, List<String> tags, Arguments arguments) {
+    public static void buildJobs(JenkinsConfig config, List<String> tags, Arguments arguments) throws IOException {
         buildJobs(config.getJobsWithTags(tags), arguments);
     }
 
-    public static void buildJobs(JenkinsConfig config, String tag, Arguments arguments) {
+    public static void buildJobs(JenkinsConfig config, String tag, Arguments arguments) throws IOException {
         buildJobs(config.getJobsWithTag(tag), arguments);
     }
 }
