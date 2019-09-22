8d95658a79c51a465b87b6279f1cd2e5d24d8781
Joshua Leger <legermjoshua@gmail.com>
2019-09-22 10:19:48 -0700

    Added parameters to jobs

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject jenkins_orchestration "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Tool for orchestrating jenkins builds"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot jenkins-orchestration.core
   :target-path "target/%s"
--- src/jenkins_orchestration/core.clj
+++ src/jenkins_orchestration/core.clj
@@ -8,27 +8,88 @@
 
 ;; Test structure
 (def jobs [
-  {:title "Calc Mac"
-   :url "http://joshatron.io/jenkins/Calc_Mac"
-   :tags ["mac" "calc"]}
-  {:title "Calc Win"
-   :url "http://joshatron.io/jenkins/Calc_Win"
-   :tags ["win" "calc"]}
-  {:title "Calc Linux"
-   :url "http://joshatron.io/jenkins/Calc_Linux"
-   :tags ["linux" "calc"]}
-  {:title "UI Mac"
-   :url "http://joshatron.io/jenkins/UI_Mac"
-   :tags ["mac" "ui"]}
-  {:title "UI Win"
-   :url "http://joshatron.io/jenkins/UI_Win"
-   :tags ["win" "ui"]}
-  {:title "UI Linux"
-   :url "http://joshatron.io/jenkins/UI_Linux"
-   :tags ["linux" "ui"]}])
+           {:title      "Calc Mac"
+            :url        "http://joshatron.io/jenkins/Calc_Mac"
+            :tags       ["mac" "calc"]
+            :parameters [{
+                          :name  "env"
+                          :type  "string"
+                          :value "e2e"}
+                         {
+                          :name  "module"
+                          :type  "string"
+                          :value "all"}]}
+           {:title      "Calc Win"
+            :url        "http://joshatron.io/jenkins/Calc_Win"
+            :tags       ["win" "calc"]
+            :parameters [{
+                          :name  "env"
+                          :type  "string"
+                          :value "e2e"}
+                         {
+                          :name  "module"
+                          :type  "string"
+                          :value "all"}]}
+           {:title      "Calc Linux"
+            :url        "http://joshatron.io/jenkins/Calc_Linux"
+            :tags       ["linux" "calc"]
+            :parameters [{
+                          :name  "env"
+                          :type  "string"
+                          :value "e2e"}
+                         {
+                          :name  "module"
+                          :type  "string"
+                          :value "all"}]}
+           {:title      "UI Mac"
+            :url        "http://joshatron.io/jenkins/UI_Mac"
+            :tags       ["mac" "ui"]
+            :parameters [{
+                          :name  "env"
+                          :type  "string"
+                          :value "e2e"}
+                         {
+                          :name  "module"
+                          :type  "string"
+                          :value "all"}]}
+           {:title      "UI Win"
+            :url        "http://joshatron.io/jenkins/UI_Win"
+            :tags       ["win" "ui"]
+            :parameters [{
+                          :name  "env"
+                          :type  "string"
+                          :value "e2e"}]}
+           {:title      "UI Linux"
+            :url        "http://joshatron.io/jenkins/UI_Linux"
+            :tags       ["linux" "ui"]
+            :parameters [{
+                          :name  "env"
+                          :type  "string"
+                          :value "e2e"}
+                         {
+                          :name  "submodule"
+                          :type  "string"
+                          :value ""}]}])
+
+(def params [
+             {:name "env"
+              :value "dev"}
+             {:name "submodule"
+              :value "first"}])
 
 (defn filter-tag [jobs tag]
   (filter (fn [job] (some #(= tag %1) (:tags job))) jobs))
 
 (defn filter-tags [jobs tags]
   (reduce filter-tag jobs tags))
+
+(defn replace-parameters [jobs new-params]
+  (map (fn [job]
+         (reduce (fn [job new-param]
+                   (assoc job :parameters
+                              (map (fn [old-param]
+                                     (if (= (:name old-param)(:name new-param))
+                                       (assoc old-param :value (:value new-param))
+                                       old-param))
+                                   (:parameters job))))
+                 job new-params)) jobs))
