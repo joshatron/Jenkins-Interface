4fcfc3754610b4b8a63d7fdaeb759e6442f5370d
Joshua Leger <legermjoshua@gmail.com>
2019-10-12 10:11:12 -0700

    Made a bunch of small improvements

--- src/jenkins_orchestration/http.clj
+++ src/jenkins_orchestration/http.clj
@@ -12,6 +12,15 @@
              {:basic-auth [username token]}))
     true))
 
+(defn- get-job-build-body
+  "Get info about the job from the server"
+  [base-url build username token]
+  (json/parse-string
+    (:body (client/get
+             (str base-url "/" build "/api/json")
+             {:basic-auth [username token]}))
+    true))
+
 (defn- get-parameters-from-body
   "Collect a list of parameters from the body of a job"
   [body]
@@ -40,41 +49,45 @@
                          {:basic-auth [username token]}))
                 true))))
 
-(defn get-children
+(defn- get-children
   "Get the urls of all the children jobs in a job"
-  [base-url servers]
-  (let [server (jfilter/get-server-for-url servers base-url)]
+  [base-url config]
+  (let [server (jfilter/get-server-for-url (:servers config) base-url)]
     (get-children-for-url base-url (:username server) (:token server))))
 
-(defn create-job
+(defn add-job
   "Create a job from the url, name, and tags"
-  [base-url servers tags]
-  (let [server (jfilter/get-server-for-url servers base-url)
-        body (get-job-body base-url (:username server) (:token server))]
-    {:title (:name body)
-     :url base-url
-     :tags tags
-     :parameters (get-parameters-from-body body)}))
+  [base-url config tags]
+  (assoc config :jobs (concat (:jobs config)
+                              (let [server (jfilter/get-server-for-url (:servers config) base-url)
+                                    body (get-job-body base-url (:username server) (:token server))]
+                                {:title (:name body)
+                                 :url base-url
+                                 :tags tags
+                                 :parameters (get-parameters-from-body body)}))))
 
-(defn create-jobs-from-children
+(defn add-jobs-from-children
   "Creates jobs from the children of a folder"
-  [base-url servers tags]
-  (map #(create-job %1 servers tags) (get-children base-url servers)))
+  [base-url config tags]
+  (map #(add-job %1 (:servers config) tags) (get-children base-url (:servers config))))
 
 (defn trigger-build
   "Trigger a build on the specified job"
-  [job servers new-params]
-  (let [server (jfilter/get-server-for-job servers job)]
+  [job config new-params]
+  (let [server (jfilter/get-server-for-job (:servers config) job)]
     (trigger-build-for-url (:url job) (:username server) (:token server) (:parameters (jfilter/inject-parameters job new-params)))))
 
 (defn trigger-builds
   "Trigger a build on all of the specified jobs"
   [config new-params]
-  (doseq [job (:jobs config)] (trigger-build job (:servers config) new-params)))
+  (doseq [job (:jobs config)] (trigger-build job config new-params)))
 
 (defn get-last-finished-durations
   "Get last duration of jobs specified"
   [config]
   (map #(:duration (let [server (jfilter/get-server-for-url (:servers config) (:url %1))]
-                     (get-job-body (str (:url %1) "/" (:number (:lastCompletedBuild (get-job-body (:url %1) (:username server) (:token server)))))
-                                   (:username server) (:token server)))) (:jobs config)))
+                     (get-job-build-body (:url %1)
+                                         (:number (:lastCompletedBuild (get-job-body (:url %1) (:username server) (:token server))))
+                                         (:username server)
+                                         (:token server))))
+       (:jobs config)))
